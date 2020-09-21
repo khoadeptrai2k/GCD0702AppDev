90d07535fa32541cc4237ecaa947cfcd6b3f33ed
vinhhn3 <vinhhn3@fe.edu.vn>
2020-09-21 08:36:06 +0700

    Add Index action of ProductsController

---
+++ GCD0702AppDev/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using GCD0702AppDev.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GCD0702AppDev.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private ApplicationDbContext _context;
+
+		public ProductsController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		// GET: Products
+		public ActionResult Index()
+		{
+			var products = _context.Products.ToList();
+			return View(products);
+		}
+	}
+}
