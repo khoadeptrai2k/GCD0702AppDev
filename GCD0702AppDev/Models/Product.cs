32e8cbf958daca8476dc6e61d9849f9f8d97231b
vinhhn3 <vinhhn3@fe.edu.vn>
2020-09-21 08:05:28 +0700

    Add Product model

---
+++ GCD0702AppDev/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GCD0702AppDev.Models
+{
+	public class Product
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public double Price { get; set; }
+	}
+}
