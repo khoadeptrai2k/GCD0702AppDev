9e8639ee47a409f8cd898c934d8d3ff483fb8058
Vinh Hoang Nhu (FE FGW DN) <VinhHN3@fe.edu.vn>
2020-10-08 09:01:40 +0700

    Implement POST action of Products Api

--- GCD0702AppDev/Controllers/Api/ProductsController.cs
+++ GCD0702AppDev/Controllers/Api/ProductsController.cs
@@ -2,6 +2,7 @@
 using GCD0702AppDev.Models;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web.Http;
 
 namespace GCD0702AppDev.Controllers.Api
@@ -58,6 +59,38 @@ namespace GCD0702AppDev.Controllers.Api
 			_context.SaveChanges();
 
 			return Ok("Product with Id " + product.Id + " deleted...");
+		}
+
+		[HttpPost]
+		public IHttpActionResult Create(Product product)
+		{
+			var doesProductExist = _context.Products.Any(p => p.Name.Contains(product.Name));
+
+			if (doesProductExist)
+				return BadRequest();
+
+			var categoryInDb = _context.Categories.SingleOrDefault(c => c.Id == product.CategoryId);
+
+			if (categoryInDb == null)
+				return BadRequest();
+
+			var newProduct = new Product
+			{
+				Name = product.Name,
+				Price = product.Price,
+				CategoryId = product.CategoryId
+			};
+
+			_context.Products.Add(newProduct);
+			_context.SaveChanges();
+
+			return StatusCode(HttpStatusCode.Created);
+		}
+
+		[HttpPut]
+		public IHttpActionResult EditById(int id, Product product)
+		{
+			return StatusCode(HttpStatusCode.NoContent);
 		}
 	}
 }
